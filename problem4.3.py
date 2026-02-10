a146cb8c0ecae1750bee4001c6ef747dc5e98846
siddharthatomar33 <siddharthatomar33@gmail.com>
2026-02-10 08:43:02 +0530

    first commit

---
+++ problem4.3.py
@@ -0,0 +1,11 @@
+mydict = {
+    "harry":90909090990,
+    "john Doe":898989889,
+    "donald":4545455454,
+}
+
+print(mydict.keys())
+print(mydict.values())
+
+for key, value in mydict.items():
+    print(key,value)
