ce3a940fffee27e05cd36bc307b94c45eaca353c
siddharthatomar33 <siddharthatomar33@gmail.com>
2026-02-10 09:33:59 +0530

    python-practise

---
+++ oopsprac/problem3.py
@@ -0,0 +1,20 @@
+class animal:
+    def sound(self):
+        print("animal make some sound")
+
+class dog:
+    def sound(animal):
+        print("bark")
+
+class cat(animal):
+    def sound(animal):
+        print("meow")
+
+a=animal()        
+a.sound()
+
+b=dog()
+b.sound()
+
+c=cat()
+c.sound()
